97cc222669c2081fcdad6ce7f35f00e7cbd8d4f7
Hriday Gupta <hridaygupta100@hotmail.com>
2022-09-24 23:24:35 +0530

    dummy data added

--- src/utils/dummy.js
+++ src/utils/dummy.js
@@ -2,20 +2,23 @@ export const dummy = [{
     price: 1000,
     address: 'A21 Building Block',
     location: 'New York, USA',
-    movein: new Date('2022-09-22'),
-    type: 'House'
+    movein: new Date('2022-09-22'), //date after you can move in
+    type: 'House',
+    name: 'Hill House'
 },
 {
     price: 6000,
     address: 'A21 Building Block',
     location: 'New Delhi, IN',
     movein: new Date('2022-09-20'),
-    type: 'House'
+    type: 'House',
+    name: 'Sea Mansion'
 },
 {
     price: 4000,
     address: 'A21 Building Block',
     location: 'New Delhi, IN',
     movein: new Date('2022-09-24'),
-    type: 'Office'
+    type: 'Office',
+    name: 'Golf Apartments'
 }];
