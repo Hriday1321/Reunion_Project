b758559697125e5a05bc7798b2663fe3668ba392
Hriday Gupta <hridaygupta100@hotmail.com>
2022-09-24 20:40:50 +0530

    data is here

--- src/utils/dummy.js
+++ src/utils/dummy.js
@@ -2,6 +2,20 @@ export const dummy = [{
     price: 1000,
     address: 'A21 Building Block',
     location: 'New York, USA',
-    movein: '02-02-2022',
+    movein: new Date('2022-09-22'),
     type: 'House'
+},
+{
+    price: 6000,
+    address: 'A21 Building Block',
+    location: 'New Delhi, IN',
+    movein: new Date('2022-09-20'),
+    type: 'House'
+},
+{
+    price: 4000,
+    address: 'A21 Building Block',
+    location: 'New Delhi, IN',
+    movein: new Date('2022-09-24'),
+    type: 'Office'
 }];
