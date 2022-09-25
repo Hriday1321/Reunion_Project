6ab05b89fe6310c31a621e01d2007b7b2501ddf8
Hriday Gupta <hridaygupta100@hotmail.com>
2022-09-25 13:16:44 +0530

    initial styling hehe

---
+++ src/common/common.elements.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const DivC = styled.div`
+    display: flex;
+    height: 100vh;
+`
+
+export const Side = styled.div`
+    flex: 0.2;
+    background-color: #111;
+    color: white
+`
+
+export const Main = styled.div`
+    flex: 0.8;
+    background-color: #333;
+    color: white;
+`
--- src/pages/Rent.js
+++ src/pages/Rent.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Nav from '../components/Nav/Nav'
 import Body from '../components/Body/Body'
+import { DivC, Side, Main} from '../common/common.elements'
 
 function Rent() {
   return (
-    <div>
-        <Nav />
-        <Body />
-    </div>
+    <DivC>
+      <Side><Nav /></Side>
+      <Main><Body /></Main> 
+    </DivC>
   )
 }
 
